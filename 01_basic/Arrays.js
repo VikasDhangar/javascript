dd09f6e1b184e466698bbffae1dba41189509819
VikasDhangar <vickydhangar476@gmail.com>
2024-08-11 21:21:08 +0530

    string & Arrays
    string & Arrays

---
+++ 01_basic/Arrays.js
@@ -0,0 +1,10 @@
+let Number = [1 ,2, 3, 4];
+// console.log(Number);
+Number.push(5,7,8);
+// console.log(Number);
+
+
+// console.log(Number.length);
+console.log(Number.indexof);
+
+
---
+++ 01_basic/string_interpolation.js
@@ -0,0 +1,5 @@
+let fristname = 'vikas';
+let second    = "dhangar";
+let text =`my name is ${fristname} ${second}`;
+console.log(text);
+
