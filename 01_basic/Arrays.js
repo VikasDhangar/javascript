00ee274418955ed700b72c98773f5ee564820321
VikasDhangar <vickydhangar476@gmail.com>
2024-08-12 14:18:06 +0530

    learning new
    learning new

--- 01_basic/Arrays.js
+++ 01_basic/Arrays.js
@@ -1,10 +1,55 @@
-let Number = [1 ,2, 3, 4];
-// console.log(Number);
-Number.push(5,7,8);
+// let Number = [1 ,'vikas', 3, 4];
+// console.log(Number)
+
+// Number.push('google',87)
+// console.log(Number)
+
+// Number.pop()
+// console.log(Number)
+
+// Number.shift()
+// console.log(Number)
+
+// Number.unshift('microsoft')
+// console.log(Number)
+
+// Number.slice(0,3)
 // console.log(Number);
 
+// console.log(Number)
 
-// console.log(Number.length);
-console.log(Number.indexof);
+// Number.splice (2,0,`flipkart`)
+// console.log(Number)
+
+
+
+// const are = [6,8,5,9,2,5,8,]
+// console.log(are[4])
+// let vikas = are.length
+// console.log(vikas);
+
+// console.log(are.length);
+
+// console.log(are);
+
+// are.sort()
+// console.log(are)
+
+// are.find([2,4])
+// console.log(are);
+
+// function vikas(age,height) {
+//     console.log(age,height)
+  
+// }
+// vikas(28,5.2)
+
+
+
+
+
+
+
+
 
 
--- 01_basic/string_interpolation.js
+++ 01_basic/string_interpolation.js
@@ -1,5 +1,17 @@
-let fristname = 'vikas';
-let second    = "dhangar";
-let text =`my name is ${fristname} ${second}`;
-console.log(text);
+// let fristname = 'vikas';
+// let second    = "dhangar";
+// let text =`my name is ${fristname} ${second}`;
+// console.log(text);
 
+// let String = "my name is vicky"
+// // console.log(String.length)
+//  let substring = String.substring(4);
+//  console.log(substring);
+  let sentance = "hello ji kese ho sare "
+ console.log(sentance);
+ let fWord = sentance.split(` `)
+ console.log(fWord);
+ 
+ 
+
+ 
